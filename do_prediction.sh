0d5b8037e6db20b1cea437e6e87dca03425db18e
sl394@duke.edu <sl394@duke.edu>
2019-09-17 20:41:15 -0400

    do prediction

--- do_prediction.sh
+++ do_prediction.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 GLUE_DIR="glue_data"
 STORAGE_BUCKET="gs://my-bert-bucket"
 MY_CONFIG_DIR="gs://my-bert-bucket/uncased_L-12_H-768_A-12"
 TASK_NAME=("WNLI" "RTE" "MRPC" "CoLA" "SST" "QNLI" "MNLI")
 TASK_TRAIN_STEP=("59" "233" "343" "801" "6313" "9819" "36815")
-
-
 
 for ((i=0 ; i<=${#TASK_NAME[@]}; i++ )); do
 	python3 ./RevisedBert/run_classifier.py \

b9ad8b07e28908bd99ef0bd465ef5e9eb7f8aeb2
sl394@duke.edu <sl394@duke.edu>
2019-09-18 22:19:41 -0400

    debug

--- run_sts.py
+++ run_sts.py
@@ -193,13 +193,13 @@ class StsProcessor(object):
   def get_dev_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "dev_matched.tsv")),
+        self._read_tsv(os.path.join(data_dir, "dev.tsv")),
         "dev_matched")
 
   def get_test_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "test_matched.tsv")), "test")
+        self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
 
 
   def _create_examples(self, lines, set_type):

9b7c080f97dd7976dc95167cde34da6e985466ab
sl394@duke.edu <sl394@duke.edu>
2019-09-22 16:10:14 -0400

    indentation

--- run_classifier.py
+++ run_classifier.py
@@ -1182,11 +1182,12 @@ def main(_):
 		output_query_file = os.path.join(FLAGS.output_dir, "query.tsv")
 		output_key_file = os.path.join(FLAGS.output_dir, "key.tsv")
 
+		'''
 		for i,prediction in enumerate(result):
 			for the_key in prediction:
 				tf.logging.info("keys in the output: \n")
 				tf.logging.info(the_key + "\n")
-
+		'''
 		'''
 		with tf.gfile.GFile(output_predict_file, "w") as writer:
 			num_written_lines = 0
@@ -1218,7 +1219,7 @@ def main(_):
 				if i >= num_actual_predict_examples:
 					break
 				tf.logging.info("enter into the query \n")
-				
+
 				query_filter = prediction["query_filter"]
 				(from_length,hsize) = query_filter.size()
 

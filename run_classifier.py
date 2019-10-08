f050ecb8f1257dcffde193e0cbbd5d61ce37f547
sl394@duke.edu <sl394@duke.edu>
2019-10-08 17:06:45 -0400

    debug

--- run_classifier.py
+++ run_classifier.py
@@ -925,8 +925,8 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
 					#predictions={"probabilities": probabilities},
 					#predictions={"query_filter": query_filter},
 					#predictions={"key_filter": key_filter},
-					#predictions={"attention_filters": attention_filters},
-					predictions={"attention_scores": attention_scores},
+					predictions={"attention_filters": attention_filters},
+					#predictions={"attention_scores": attention_scores},
 					scaffold_fn=scaffold_fn)
 		return output_spec
 
@@ -1256,7 +1256,7 @@ def main(_):
 				writer.write("\n\n")
 		
 		
-		'''
+		
 		with tf.gfile.GFile(output_att_score_file, "w") as writer:
 			tf.logging.info("enter into the writer \n")
 			for (i, prediction) in enumerate(result):
@@ -1293,7 +1293,7 @@ def main(_):
 						vec_line = "\t".join([str(num) for num in word]) + "\n\n"
 						writer.write(vec_line)
 				writer.write("\n\n")
-		'''
+		
 
 		
 
